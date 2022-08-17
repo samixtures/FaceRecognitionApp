8e7bb23bf30ae1293517136a0fd8b46e6bf0df43
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-16 18:18:39 -0700

    Made "App.js" a container rather than a component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
@@ -6,21 +7,22 @@ import Rank from './components/Rank/Rank'
 import Particles from './components/Particles'
 
 
-
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-      <Particles id="tsparticles" />
-{/* {      <Navigation/>
-      <Logo/>
-      <ImageLinkForm/>
-      <FaceRecognition/>} */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm/>
+        <Particles id="tsparticles" />
+  {/* {      <Navigation/>
+        <Logo/>
+        <ImageLinkForm/>
+        <FaceRecognition/>} */}
+      </div>
+    );
+  }
 }
 
 export default App;
