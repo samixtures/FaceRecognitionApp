e5ab21a80824c86d021c35e87e8562df3b522717
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-16 18:27:49 -0700

    "onInputChange" detects changes in the input box

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ import Particles from './components/Particles'
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
   render() {
     return (
       <div className="App">
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm onInputChange={this.onInputChange}/>
         <Particles id="tsparticles" />
   {/* {      <Navigation/>
         <Logo/>
