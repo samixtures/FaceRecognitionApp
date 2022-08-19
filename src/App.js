9e02b658f7de187cb9ad1c1b6269ca22827485a8
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-19 09:25:16 -0700

    Added Clarifai sample code, made FaceRecognition

--- src/App.js
+++ src/App.js
@@ -4,7 +4,56 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from './components/Particles'
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, app ID, model details, and the URL
+    // of the image we want as an input. Change these strings to run your own example.
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    const USER_ID = 'samixtures';
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'f69d3700448f4e23b9af49398d30fe47';
+    const APP_ID = 'my-first-application';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'general-image-recognition';
+    const MODEL_VERSION_ID = 'aa7f35c01e0642fda5cf400f543e7c40';    
+    const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
 
 
 class App extends Component {
@@ -19,7 +68,12 @@ class App extends Component {
   }
   onButtonSubmit = () => {
     console.log("click")
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
   }
+  
   render() {
     return (
       <div className="App">
@@ -30,6 +84,7 @@ class App extends Component {
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}
         />
+        <FaceRecognition/>
         <Particles id="tsparticles" />
   {/* {      <Navigation/>
         <Logo/>
@@ -39,5 +94,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
